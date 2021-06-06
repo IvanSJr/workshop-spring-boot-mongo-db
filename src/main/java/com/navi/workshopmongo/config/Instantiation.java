ddb93cc0ffe85e6e1004d9f73517e25f93f12247
Ivan Júnior <ivan.junior2706@gmail.com>
2021-06-05 22:22:25 -0300

    Operação de instanciação da base de dado.

---
+++ src/main/java/com/navi/workshopmongo/config/Instantiation.java
@@ -0,0 +1,32 @@
+package com.navi.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.navi.workshopmongo.domain.User;
+import com.navi.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner{
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll();
+		
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		User ivan = new User(null, "Ivan Júnior", "ivanjunior@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob, ivan));
+	}
+
+}
