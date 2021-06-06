0940fad00eed921b45833eae471b4d98cc483c5b
IvanSJr <ivan.junior2706@gmail.com>
2021-06-06 19:38:53 -0300

    Atualização de usuários com PUT.

--- src/main/java/com/navi/workshopmongo/services/UserService.java
+++ src/main/java/com/navi/workshopmongo/services/UserService.java
@@ -30,6 +30,18 @@ public class UserService {
 		return repo.insert(obj);
 	}
 	
+	public User update(User obj) {
+	    Optional<User> obj1 = repo.findById(obj.getId());
+	    User newObj = obj1.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	    updateData(newObj, obj);
+	    return repo.save(newObj);
+		}
+	
+	private void updateData(User newObj, User obj) {
+		newObj.setEmail(obj.getEmail());
+		newObj.setName(obj.getName());
+	}
+
 	public void delete(String id) {
 		findById(id);
 		repo.deleteById(id);
